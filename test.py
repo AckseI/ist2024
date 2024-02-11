c65abe6b2ac0b7281a522383da737e8b01958222
AckseI <36826218+AckseI@users.noreply.github.com>
2024-02-11 19:08:11 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+def print_sum(a, b):
+    return print(a + b)
+
+def main():
+    a = int(input("Введите первое число: "))
+    b = int(input("Введите второе число: "))
+    print_sum(a, b)
+
+
+main()
